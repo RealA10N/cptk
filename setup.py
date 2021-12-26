a5463fb26c26688b2f6621023882de92273dfd9e
Alon's Ubuntu Laptop <downtown2u@gmail.com>
2021-12-26 19:26:40 +0200

    👷 Added basic `setup.py` script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r", encoding="utf8") as f:
+    README = f.read()
+
+with open("requirements.txt", mode="r", encoding="utf8") as f:
+    DEPENDENCIES = f.read().splitlines()
+
+setup(
+    name="cptk",
+    version="0.1.0",
+    description="Your personal assistant for everything competitive programming related!",
+    python_requires=">=3.6,<4",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Alon Krymgand Osovsky",
+    author_email="downtown2u@gmail.com",
+    packages=find_packages(),
+    install_requires=DEPENDENCIES,
+)
