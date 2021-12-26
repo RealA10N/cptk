d547beea273e8c0959a4f3a60a2b8ec985c72f34
Alon's Ubuntu Laptop <downtown2u@gmail.com>
2021-12-26 18:02:32 +0200

    ⚡️ Website methods are now `classmethod`s

--- cptk/website.py
+++ cptk/website.py
@@ -22,39 +22,39 @@ class Website(ABC):
     Has different methods that can fetch data and information from the website.
     """
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def name() -> str:
+    def name(cls) -> str:
         """ The name of the website. """
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def domain() -> Union[str, List[str]]:
+    def domain(cls) -> Union[str, List[str]]:
         """ A single domain or a list of domains that are represented by this
         class. """
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def is_contest(info: PageInfo) -> bool:
+    def is_contest(cls, info: PageInfo) -> bool:
         """ Returns True only if the given page information describes a page
         with a contest (a collection of multiple problems). """
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def to_contest(info: PageInfo) -> Optional['Contest']:
+    def to_contest(cls, info: PageInfo) -> Optional['Contest']:
         """ Constructs and a Contest instance that represents the contest that
         is displayed in the given page. If the given page doesn't display a
         contest information, returns None. """
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def is_problem(info: PageInfo) -> bool:
+    def is_problem(cls, info: PageInfo) -> bool:
         """ Returns True only if the given page information describes a page
         with a single problem. """
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def to_problem(info: PageInfo) -> Optional['Problem']:
+    def to_problem(cls, info: PageInfo) -> Optional['Problem']:
         """ Constructs and a Problem instance that represents the problem that
         is displayed in the given page. If the given page doesn't display a
         single and unique problem, returns None. """
