49ff6ecca548a3d9e1bb7850b23185b1542f5793
Alon's Ubuntu Laptop <downtown2u@gmail.com>
2021-12-27 10:08:04 +0200

    🐛 Added missing import for typehints

--- cptk/websites/codeforces.py
+++ cptk/websites/codeforces.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import List, Optional
     from cptk import PageInfo
+    from bs4 import BeautifulSoup
 
 
 class Codeforces(Website):
