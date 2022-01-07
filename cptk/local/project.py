17825b256dc8fad25184cc424c86e9aa1eb01704
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-08 01:23:38 +0200

    💡 Added comments to `LocalProject.init` method

--- cptk/local/project.py
+++ cptk/local/project.py
@@ -3,7 +3,7 @@ from shutil import copytree, rmtree
 
 from cptk.utils import cached_property
 from cptk.core import Configuration
-from cptk.templates import DEFAULT_TEMPLATES
+from cptk.templates import Template, DEFAULT_TEMPLATES
 from cptk.constants import (
     PROJECT_FILE,
     DEFAULT_TEMPLATE_FOLDER,
@@ -22,31 +22,45 @@ class LocalProject:
     def __init__(self, location: str) -> None:
         self.location = location
 
+    @staticmethod
+    def _copy_template(template: Template, dst: str) -> None:
+        """ Copies the given template to the destination path. """
+
+        if os.path.exists(dst):
+            rmtree(dst)
+        copytree(src=template.path, dst=dst)
+
     @classmethod
     def init(cls: Type[T], location: str, template: str = None) -> T:
-        UID_TO_TEMPLATE = {
-            template.uid: template
-            for template in DEFAULT_TEMPLATES
-        }
+        """ Initialize an empty local project in the given location with the
+        given properties and settings. Returns the newly created project as a
+        LocalProject instance. """
+
+        # Create the directory if it doesn't exist
+        os.makedirs(location, exist_ok=True)
 
         if template is None:
             template = DEFAULT_TEMPLATE_FOLDER
 
-        elif template in UID_TO_TEMPLATE:
-            default_template = UID_TO_TEMPLATE[template]
+        # If the given template is actually one of the predefined template names
+        temp_obj = {t.uid: t for t in DEFAULT_TEMPLATES}.get(template)
+        if temp_obj is not None:
+            dst = os.path.join(location, DEFAULT_TEMPLATE_FOLDER)
+            cls._copy_template(temp_obj, dst)
             template = DEFAULT_TEMPLATE_FOLDER
 
-            # copy default template to current project
-            src = default_template.path
-            dst = os.path.join(location, DEFAULT_TEMPLATE_FOLDER)
-            if os.path.exists(dst):
-                rmtree(dst)
-            copytree(src, dst)
+        # Now 'template' actually has the path to the template folder.
+        # Create the template folder if it doesn't exist yet
+        os.makedirs(template, exist_ok=True)
 
+        # Create the project configuration instance and dump it into a YAML
+        # configuration file.
         config = ProjectConfig(template=template)
         config_path = os.path.join(location, PROJECT_FILE)
         config.dump(config_path)
 
+        # We have created and initialized everything that is required for a
+        # cptk project. Now we can create a LocalProject instance and return it.
         return cls(location)
 
     @cached_property
