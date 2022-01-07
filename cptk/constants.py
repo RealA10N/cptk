bb5e04a0cd793616669e998782624debf6ce6f83
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-08 00:52:08 +0200

    ⚡️ Default templates are now being copied to project

--- cptk/constants.py
+++ cptk/constants.py
@@ -3,4 +3,4 @@ RECIPE_NAME = 'recipe.cptk.yaml'
 PROJECT_CONFIG_NAME = 'project.cptk.yaml'
 TESTS_FOLDER_NAME = 'tests'
 
-DEFAULT_TEMPLATE_FOLDER = 'TEMPLATE'
+DEFAULT_TEMPLATE_FOLDER = '.cptk/template'
--- cptk/local/project.py
+++ cptk/local/project.py
@@ -1,7 +1,9 @@
 import os
+from shutil import copytree, rmtree
 
 from cptk.utils import cached_property
 from cptk.core import Configuration
+from cptk.templates import DEFAULT_TEMPLATES
 from cptk.constants import (
     CPTK_FOLDER_NAME,
     PROJECT_CONFIG_NAME,
@@ -23,8 +25,24 @@ class LocalProject:
 
     @classmethod
     def init(cls: Type[T], location: str, template: str = None) -> T:
+        UID_TO_TEMPLATE = {
+            template.uid: template
+            for template in DEFAULT_TEMPLATES
+        }
+
         if template is None:
             template = DEFAULT_TEMPLATE_FOLDER
+
+        elif template in UID_TO_TEMPLATE:
+            default_template = UID_TO_TEMPLATE[template]
+            template = DEFAULT_TEMPLATE_FOLDER
+
+            # copy default template to current project
+            src = default_template.path
+            dst = os.path.join(location, DEFAULT_TEMPLATE_FOLDER)
+            if os.path.exists(dst):
+                rmtree(dst)
+            copytree(src, dst)
 
         config = ProjectConfig(template=template)
 
