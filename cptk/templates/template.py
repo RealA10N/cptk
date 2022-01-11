23c90c103df278d9c479bcf38c2cd57e2cd4b558
Alon's Ubuntu Laptop <downtown2u@gmail.com>
2022-01-11 11:30:02 +0200

    🚚 Moved default templates to `defaults`

--- cptk/templates/template.py
+++ cptk/templates/template.py
@@ -10,26 +10,28 @@ class Template:
 
 
 HERE = os.path.dirname(__file__)
+SRC = os.path.join(HERE, '..')
+TEMPLATES_FOLDER = os.path.join(SRC, 'defaults', 'templates')
 
 DEFAULT_TEMPLATES = (
     Template(
         uid='g++',
         name='C++ (Compiled with g++)',
-        path=os.path.join(HERE, 'defaults', 'g++'),
+        path=os.path.join(TEMPLATES_FOLDER, 'g++'),
     ),
     Template(
         uid='win-g++',
         name='C++ (Compiled with g++, Windows)',
-        path=os.path.join(HERE, 'defaults', 'win-g++'),
+        path=os.path.join(TEMPLATES_FOLDER, 'win-g++'),
     ),
     Template(
         uid='py',
         name='Python 3',
-        path=os.path.join(HERE, 'defaults', 'py'),
+        path=os.path.join(TEMPLATES_FOLDER, 'py'),
     ),
     Template(
         uid='win-py',
         name='Python 3 (Windows)',
-        path=os.path.join(HERE, 'defaults', 'win-py'),
+        path=os.path.join(TEMPLATES_FOLDER, 'win-py'),
     ),
 )
