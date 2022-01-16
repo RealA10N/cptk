4b973d5155e6f2e1a05f9b26b669550ec692ad6a
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-16 22:55:21 +0200

    ✨ Updated scrape objects structe

--- cptk/scrape/problem.py
+++ cptk/scrape/problem.py
@@ -2,38 +2,57 @@ from dataclasses import dataclass, field
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-    from typing import List, Type, Any
+    from typing import List, Any
     from cptk.scrape import Website
+    from datetime import datetime
 
 
-class Element:
-    pass
+@dataclass(unsafe_hash=True)
+class Scraped:
+    """ Any information that is scraped using cptk is represented using
+    instanaces of the 'Scraped' object, or instances of classes that inherent
+    from it. """
 
-
-@dataclass
-class Problem(Element):
-    website: 'Type[Website]' = field(compare=True)
-
-    # required metadata
-    uid: 'Any' = field(compare=True, hash=True)
+    _uid: 'Any' = field(compare=True, hash=True)
+    website: 'Website' = field(compare=True)
     name: str = field(compare=False)
     url: str = field(compare=False)
 
-    contest: 'Contest' = field(default=None, compare=False)
+
+@dataclass(unsafe_hash=True)
+class Problem(Scraped):
+    """ A basic problem that is scraped using cptk. Most website implement their
+    own 'Problem' class which inherits from this one and can store additional
+    information. """
 
     tests: 'List[Test]' = field(default_factory=list, compare=False)
     time_limit: float = field(default=None, compare=False)     # in Seconds
     memory_limit: float = field(default=None, compare=False)   # in MB
 
 
+class ContestProblem(Problem):
+    mark: str = field(compare=False)
+    contest: 'Contest' = field(compare=False)
+
+
 @dataclass(unsafe_hash=True)
-class Test(Element):
+class ProblemGroup(Scraped):
+    pass
+
+
+@dataclass(unsafe_hash=True)
+class Problemset(ProblemGroup):
+    pass
+
+
+@dataclass(unsafe_hash=True)
+class Contest(ProblemGroup):
+    active: bool = field(default=None, compare=False)
+    start_time: 'datetime' = field(default=None, compare=False)
+    end_time: 'datetime' = field(default=None, compare=False)
+
+
+@dataclass(unsafe_hash=True)
+class Test:
     input: str
     expected: str = None
-
-
-@dataclass(unsafe_hash=True)
-class Contest(Element):
-    website: 'Type[Website]' = field(compare=True)
-    uid: 'Any' = field(compare=True)
-    name: str = field(compare=False)
