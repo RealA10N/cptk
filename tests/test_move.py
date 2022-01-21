6c99ecc0aca05dc296a385521d677c2dababd98c
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-21 12:09:45 +0200

    ✅ Added `load_moves` tests

---
+++ tests/test_move.py
@@ -0,0 +1,38 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .utils import EasyDirectory
+    from typing import Dict
+
+import pytest
+
+from cptk import constants
+from cptk.local import LocalProject
+
+
+class TestMove:
+
+    @pytest.mark.parametrize('raw, expected', (
+        (
+            'code-submission-evaluation-system::cses',
+            {
+                'code-submission-evaluation-system': 'cses'
+            },
+        ),
+        (
+            'Codeforces.com :: Codeforces'
+            'Cses.fi:: Cses',
+            {
+                'Codeforces.com': 'Codeforces',
+                'Cses.fi': 'Cses',
+            }
+        )
+    ))
+    def test_load_moves(
+        self,
+        raw: str,
+        expected: 'Dict[str, str]',
+        tempdir: 'EasyDirectory',
+    ):
+        proj = LocalProject.init(tempdir.path)
+        tempdir.create(constants.MOVE_FILE, raw)
+        proj._load_moves() == expected
