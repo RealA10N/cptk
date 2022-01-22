c25812d7f9eb56e53c1790ee38c2d972c2163c03
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-22 21:06:06 +0200

    🔥 Removed move performence tests

--- tests/test_performence.py
+++ tests/test_performence.py
@@ -1,13 +1,8 @@
 from time import perf_counter
-from typing import TYPE_CHECKING
 
 import pytest
 
-if TYPE_CHECKING:
-    from .utils import EasyDirectory, Dummy
-
 from .utils import run
-from cptk.local import LocalProject
 
 
 class time_limit:
@@ -32,25 +27,3 @@ class TestPerformance:
     def test_startup_time(self) -> None:
         with time_limit(0.05):
             run('cptk --help')
-
-    def test_lots_of_moves(
-        self,
-        tempdir: 'EasyDirectory',
-        dummy: 'Dummy',
-    ) -> None:
-        with time_limit(1):
-            proj = LocalProject.init(tempdir.path)
-            proj.config.clone.path = 'clone0/${{problem.name}}'
-
-            prob_a = dummy.get_dummy_problem()
-            prob_a.name = 'A'
-
-            proj.clone_problem(prob_a)
-
-            prob_b = dummy.get_dummy_problem()
-            prob_b.name = 'B'
-
-            for i in range(1000):
-                proj.move(f'clone{i}', f'clone{i+1}')
-
-            proj.clone_problem(prob_b)
