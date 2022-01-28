c6e9d4132a963051a7f1f94d91872ab84efa1538
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-28 22:31:25 +0200

    ✅ Added `Runner` timeout tests

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING
 import pytest
 
 from cptk.core.chef import Runner
+from cptk.core.chef import RunnerResult
+
 if TYPE_CHECKING:
     from utils import EasyDirectory
 
@@ -18,6 +20,14 @@ class Case:
 
 
 class TestRunner:
+
+    @staticmethod
+    def _compare_results(case: Case, result: RunnerResult) -> None:
+        if case.output is not None:
+            assert result.outs == case.output
+
+        if case.error is not None:
+            assert result.errs == case.error
 
     @pytest.mark.parametrize('case', (
         Case('print("Hello")', output='Hello\n'),
@@ -39,9 +49,36 @@ class TestRunner:
 
         assert result.code == 0
         assert not result.timed_out
+        self._compare_results(case, result)
 
-        if case.output is not None:
-            assert result.outs == case.output
+    @pytest.mark.parametrize('case', (
+        Case(
+            'from time import sleep\nsleep(10)\n',
+            input='', output='', error=''
+        ),
+        Case(
+            'from time import sleep\n'
+            'print("I\'m tired...", flush=True)\n'
+            'sleep(10)\n',
+            input='', output="I'm tired...\n", error='',
+        ),
+        Case(
+            'from time import sleep\nprint(input(), flush=True)\nsleep(10)\n',
+            input='hello\n', output="hello\n", error='',
+        )
+    ))
+    def test_running_python_timeout(
+        self,
+        case: Case,
+        tempdir: 'EasyDirectory',
+    ) -> None:
+        run = Runner()
+        filepath = tempdir.create(case.code, 'file.py')
+        result = run.exec(
+            f'{sys.executable} {filepath}',
+            input=case.input, timeout=0.1
+        )
 
-        if case.error is not None:
-            assert result.errs == case.error
+        assert result.code != 0
+        assert result.timed_out
+        self._compare_results(case, result)
