adeb7b95b1a68375f026ffcf2b85341d0ca0ca80
Alon (Ubuntu) <downtown2u@gmail.com>
2022-02-12 12:33:44 +0200

    📝 Added help and descriptions to commands

--- cptk/commands.py
+++ cptk/commands.py
@@ -8,8 +8,10 @@ collector = CommandCollector()
 
 collector.global_argument(
     '-W', '--work-directory',
+    help='the directory in which cptk will work in, and search for a project '
+         'inside (defaults to the current working directory).',
     dest='wd',
-    metavar='PATH',
+    metavar='DIRECTORY',
     default=getcwd(),
     type=cptk.utils.path_validator(
         dir_ok=True,
@@ -19,9 +21,15 @@ collector.global_argument(
 )
 
 
-@collector.command('init')
+@collector.command(
+    'initialize',
+    aliases=['init'],
+    help='initialize a new cptk project',
+    description='Initialize a new cptk project in the given location, '
+                'using the provided project template.',
+)
 @collector.argument('template', type=str)
-def init(wd: str, template: str):
+def initialize(wd: str, template: str):
     """ Initialize a new cptk project in the given location, using the provided
     project template. """
 
@@ -35,8 +43,17 @@ def init(wd: str, template: str):
     )
 
 
-@collector.command('clone')
-@collector.argument('url', type=cptk.utils.url_validator)
+@collector.command(
+    'clone',
+    help='clones a problem into a local cptk project',
+    description='Clones a problem from a supported website into a local cptk '
+                'project. ',
+)
+@collector.argument(
+    'url',
+    help='the problem to be cloned',
+    type=cptk.utils.url_validator
+)
 def clone(url: str):
 
     from cptk.local.project import LocalProject
@@ -49,7 +66,12 @@ def clone(url: str):
     System.echo(prob.location)
 
 
-@collector.command('last')
+@collector.command(
+    'last',
+    help='outputs the last problem touched by cptk',
+    description='Outputs the absolute location of that last problem that the '
+                'cptk CLI interacted with.',
+)
 def last():
     """ Print the location of the last problem cptk interacted with. """
 
@@ -63,7 +85,12 @@ def last():
         System.echo(last)
 
 
-@collector.command('move', aliases=['mv'])
+@collector.command(
+    'move',
+    aliases=['mv'],
+    help='moves a cloned problem to a new location',
+    description='Moves a cloned problem to a new location.',
+)
 @collector.argument(
     'src',
     type=cptk.utils.path_validator(dir_ok=True, file_ok=False, must_exist=True),
