e4ac64099577856037f308a74062b68784e18e47
Alon (Ubuntu) <downtown2u@gmail.com>
2022-02-15 20:27:04 +0200

    ✅ Updated last command tests

--- tests/test_last.py
+++ tests/test_last.py
@@ -1,24 +1,24 @@
 import os
 from typing import TYPE_CHECKING
 
+import cptk.constants
+from cptk.core.templates import DEFAULT_TEMPLATES
 from cptk.local.project import LocalProject
 
 if TYPE_CHECKING:
     from .utils import EasyDirectory, Dummy
 
+TEMPLATE = DEFAULT_TEMPLATES[0].uid
 
-from cptk import constants
+
+def test_new_project(tempdir: 'EasyDirectory'):
+    proj = LocalProject.init(tempdir.path, TEMPLATE)
+    assert proj.last() is None
+    assert not os.path.isfile(tempdir.join(cptk.constants.LAST_FILE))
 
 
-class TestLast:
-
-    def test_new_project(self, tempdir: 'EasyDirectory'):
-        proj = LocalProject.init(tempdir.path)
-        assert proj.last is None
-        assert not os.path.isfile(tempdir.join(constants.LAST_FILE))
-
-    def test_after_clone(self, tempdir: 'EasyDirectory', dummy: 'Dummy'):
-        proj = LocalProject.init(tempdir.path)
-        prob = proj.clone_problem(dummy.get_dummy_problem())
-        assert os.path.isfile(tempdir.join(constants.LAST_FILE))
-        assert proj.last == prob.location
+def test_after_clone(tempdir: 'EasyDirectory', dummy: 'Dummy'):
+    proj = LocalProject.init(tempdir.path, TEMPLATE)
+    prob = proj.clone_problem(dummy.get_dummy_problem())
+    assert os.path.isfile(tempdir.join(cptk.constants.LAST_FILE))
+    assert prob == proj.last()
