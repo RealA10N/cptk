22e93a7af4d419731eb5585f4125f79f0da20a53
Alon (Ubuntu) <downtown2u@gmail.com>
2022-02-15 21:14:59 +0200

    🐛 Added Windows support for util tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,21 +12,25 @@ def test_find_tree_files(tempdir: EasyDirectory):
     os.mkdir(tempdir.join('emptydir'))
 
     res = set(find_tree_files(tempdir.path))
-    assert res == {tempdir.join(a) for a in {'a/b/c.txt', 'a/b.txt', 'a.txt'}}
+    assert res == {tempdir.join(a) for a in {
+        os.path.join('a', 'b', 'c.txt'),
+        os.path.join('a', 'b.txt'),
+        'a.txt'
+    }}
 
 
 def test_find_common_files(tempdir: EasyDirectory):
     from cptk.utils import find_common_files
 
     os.makedirs(tempdir.join('a', 'a'))
-    tempdir.create('', 'a/b/c.txt')
-    tempdir.create('', 'b/b/c.txt')
+    tempdir.create('', 'a', 'b', 'c.txt')
+    tempdir.create('', 'b', 'b', 'c.txt')
 
-    tempdir.create('', 'a/a.txt')
-    tempdir.create('', 'b/b.txt')
+    tempdir.create('', 'a', 'a.txt')
+    tempdir.create('', 'b', 'b.txt')
 
-    tempdir.create('text a', 'a/t.txt')
-    tempdir.create('text b', 'b/t.txt')
+    tempdir.create('text a', 'a', 't.txt')
+    tempdir.create('text b', 'b', 't.txt')
 
     res = set(find_common_files(tempdir.join('a'), tempdir.join('b')))
-    assert res == {'b/c.txt', 't.txt'}
+    assert res == {os.path.join('b', 'c.txt'), 't.txt'}
